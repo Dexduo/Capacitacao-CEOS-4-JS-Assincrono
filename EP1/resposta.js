0d6716505d9e6203f0f850b997e03a8e0362710a
José Eduardo <eduardosoaresn3@gmail.com>
2019-08-19 13:30:06 -0300

    Exercicio 1 feito

--- EP1/resposta.js
+++ EP1/resposta.js
@@ -1,2 +1,1 @@
-Você pode deletar ou editar esse arquivo para que esta pasta contenha 
-o conteúdo que responde o exercício proposto correspondente.
+setInterval(() => {console.log(new Date().getTime())}, 1000);
--- EP2/resposta.js
+++
@@ -1,2 +0,0 @@
-Você pode deletar ou editar esse arquivo para que esta pasta contenha 
-o conteúdo que responde o exercício proposto correspondente.
